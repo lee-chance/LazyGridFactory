88438953a845bad8ba925d13af6e2808b23de40f
Changsu Lee <729mail2@gmail.com>
2022-03-05 18:00:53 +0900

    Initial Commit

---
+++ LazyGridFactory/LazyGridFactoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGridFactoryApp.swift
+//  LazyGridFactory
+//
+//  Created by Changsu Lee on 2022/03/05.
+//
+
+import SwiftUI
+
+@main
+struct LazyGridFactoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
